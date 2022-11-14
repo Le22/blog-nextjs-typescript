6cfc04d7f5d4790da6870fba089e6c148af933f5
Louis-Étienne Girard <contact@louisetiennegirard.fr>
2022-11-14 17:32:34 +0100

    Update Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,18 +1,16 @@
 import Link from "next/link"
 
+// Header
 export default function Header() {
     return (
         <header className="header">
             <Link href={"/"} className="header__title">
-                <a
-                    className="header__title"
-                    title="Louis-Étienne Girard Développeur Front-End React"
-                >
-                    Louis-Étienne Girard
+                <a className="header__title" title="NextBlog">
+                    NextBlog
                 </a>
             </Link>
             <Link href={"/blog"}>
-                <a className="header__link">Le Blog</a>
+                <a className="header__link">The Blog</a>
             </Link>
         </header>
     )
